f8fd1d6ba15fc70916ff9032cd3780b93a730191
muzzletov <m.velimirov@gmail.com>
2021-11-14 00:27:35 +0100

    Update intercepteur.js

--- intercepteur.js
+++ intercepteur.js
@@ -5,21 +5,37 @@
 */
 
   (() => {
-     
+
     console.log("-*-INTERCEPTEUR LOADED-*-");
+    
+    /**
+     * TÖDÖ: implement granular urlMap, differentiate between HTTP METHODS > URLS > HEADERS
+     * 
+     */
+    
+    const urlMap = new Map();
+    
+    urlMap.set("https://www.google.de", {});
 
     class XMLHttpRequestIntercepteur extends XMLHttpRequest {
-      static _XMLHttpRequest = XMLHttpRequest;
-
-      constructor() {
+      static _XMLHttpRequest = XMLHttpRequest; // Huh?!
+      
+      fakeCall = false;
+      fakeResult = null;
+      fakeStatus;
+      
+      listeners = new Map();
+    constructor() {
         super();
       }
 
       addEventListener(eventName, func) {
+        this.listeners.set(eventName, func);
         super.addEventListener(eventName, (...e)=>func(...e));
       }
 
       removeEventListener(eventName, func) {
+        this.listeners.delete(eventName);
         super.removeEventListener(eventName, (...e)=>func(...e));
       }
 
@@ -30,61 +46,68 @@
       getAllResponseHeaders() { return super.getAllResponseHeaders(); }
 
       set ontimeout(callback) {
+        if(this.fakeCall) this.listeners.set("timeout", callback);
         super.ontimeout = (e)=>callback(e);
       }
 
       set onerror(callback) {
+        if(this.fakeCall) this.listeners.set("error", callback);
         super.onerror = (e)=>callback(e);
       }
 
       set onabort(callback) {
+        if(this.fakeCall) this.listeners.set("abort", callback);
         super.onabort = (e)=>callback(e);
       }
 
-      set onloadstart(callback) {
-        super.onloadstart = (e)=>callback(e);
-      }
-
       set onload(callback) {
+        if(this.fakeCall) this.listeners.set("load", callback);
         super.onload = (e)=>callback(e);
       }
 
       set onloadend(callback) {
+        if(this.fakeCall) this.listeners.set("loadend", callback);
         super.onloadend = (e)=>callback(e);
       }
 
+      set onloadstart(callback) {
+        if(this.fakeCall) this.listeners.set("loadstart", callback);
+        super.onloadstart = (e)=>callback(e);
+      }
 
       sendAsBinary(data) {
         super.sendAsBinary(data);
       }
 
       setRequestHeader(name, value) {
+      if(!this.fakeCall)
         super.setRequestHeader(name, value);
       }
 
-      set onloadstart(callback) {
-        super.onloadstart = (e)=>callback(e);
-      }
-
       open(...args) {
-        super.open(...args);
+        const key = JSON.stringify(args);
+        console.log(key);
+        console.log(args);
 
+        this.fakeResult = urlMap.get(key);
+        this.fakeCall = this.fakeResult !== undefined;
+        this.fakeStatus = this.fakeCall ? 0 : undefined;
+
+        if(!this.fakeCall) super.open(...args);
       }
 
       get status() {
-        return super.status;
-      }
-
-      get statusText() {
-        return super.statusText;
+        return this.fakeStatus ?? super.status;
       }
 
       get response() {
+        if(!this.fakeCall) console.log(super.response);
         return super.response;
       }
 
       get responseText() {
-        return super.responseText;
+        if(!this.fakeCall) console.log(super.responseText);
+        return this.fakeResult ?? super.responseText;
       }
 
       get responseURL() {
@@ -92,6 +115,7 @@
       }
 
       get responseXML() {
+        if(!this.fakeCall) console.log(super.responseXML);
         return super.responseXML;
       }
 
@@ -111,11 +135,23 @@
         return super.readyState;
       }
 
+      fakeCalls() {
+        const progressEvent = new ProgressEvent('ProgressEvent.STANDARD_OUTPUT_DATA', { });
+        (this.listeners.get("loadstart") ?? ((...args) =>{})) (this, progressEvent);
+        (this.listeners.get("loadend") ?? ((...args) =>{})) (this, progressEvent);
+        (this.listeners.get("load") ?? ((...args) =>{})) (this, progressEvent);
+      }
+
       send(body) {
-        super.send(body);
+        if(!this.fakeCall)
+          super.send(body);
+        else {
+          this.fakeStatus = 200;
+          setTimeout(this.fakeCalls.bind(this), 0);
+        }
       }
     }
-    
+
     XMLHttpRequest = XMLHttpRequestIntercepteur;
 
   })();
